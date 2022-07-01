7f33ac7302400f1726d05b52abc083f6252f2dc3
stankneee <103902887+stankneee@users.noreply.github.com>
2022-07-01 13:57:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name generator! ')
+city = input('What your favorite city?\n')
+pet = input('Whats your favortie animal\n')
+print('Your band name is ' + city + ' '+pet+'s')
